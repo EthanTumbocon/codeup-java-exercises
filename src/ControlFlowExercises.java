448e430dba77f20d2700a1e1031d72a5919c8887
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2020-12-03 13:59:23 -0600

    success!

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,11 @@
 public class ControlFlowExercises {
 
     public static void main(String[] args){
-        int i = 0;
+        int i = 100;
         do{
             System.out.println(i);
-            i = i + 2;
+            i = i - 5;
         }
-        while(i <= 100);
+        while(i >= -10);
     }
 }
