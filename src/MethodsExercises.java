35045c2f8481c91b4f1232d888bd203b3214c5c2
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2020-12-07 09:35:05 -0600

    working thru method exercises

---
+++ src/MethodsExercises.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MethodsExercises {
+
+//    public static void main(String[] args){
+//    Scanner scanner = new Scanner(System.in);
+//        int a = 62;
+//        int b = 18;
+//        System.out.println(Integer.sum(a,b));
+//    }
+//    public static void main(String[] args){
+//        int c = 50;
+//        int d = 25;
+//        System.out.println(Math.subtractExact(c,d));
+//    }
+//    public static void main(String[] args){
+//    int e = 24;
+//    int f = 32;
+//    int g = e * f;
+//    System.out.println(g);
+//    }
+    public static void main(String[] args){
+
+    }
+}
+
