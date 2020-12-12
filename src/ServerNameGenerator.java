2c8483c97130fbaac01907c753bcd09244c617dd
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2020-12-12 14:34:35 -0600

    working through the next exercise

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class ServerNameGenerator {
+    String[] adj = {"adorable", "bewildered", "cautious", "dangerous", "ecstatic", "fantastic", "gargantuan", "healthy", "inexpensive", "jittery"};
+
+    String[] noun = {"king", "lawyer", "magazine", "needle", "oyster", "pizza", "quill", "rainbow", "shampoo", "teacher" };
+
+}
