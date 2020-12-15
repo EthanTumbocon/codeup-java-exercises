23e1cbf11cb43b13422f9dbc787a201a9dc3c254
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2020-12-15 15:55:02 -0600

    finished with the exercise

--- src/Shapes/Rectangle.java
+++ src/Shapes/Rectangle.java
@@ -1,23 +1,29 @@
 package Shapes;
 
-public class Rectangle {
+public class Rectangle extends Quadrilateral implements Measurable {
     protected double length;
     protected double width;
 
     public Rectangle (double length, double width){
+        super(length,width);
+    }
+    @Override
+    public void setLength(double length){
         this.length = length;
+    }
+    @Override
+    public void setWidth(double width){
         this.width = width;
     }
-
+    @Override
     public double getArea(){
+        System.out.println("The area of the rectangle is: ");
         return length * width;
     }
-
+    @Override
     public double getPerimeter(){
+        System.out.println("The perimeter of the rectangle is: ");
         return 2 * length + 2 * width;
     }
-
-
-
 
 }
---
+++ src/Shapes/Shape.java
@@ -0,0 +1,7 @@
+package Shapes;
+
+abstract class Shape {
+
+}
+
+
