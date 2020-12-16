a1fcc1b3fe634246cfc89a657393050bf4c4caa3
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2020-12-16 10:31:30 -0600

    finished arraylists

--- src/Shapes/Rectangle.java
+++ src/Shapes/Rectangle.java
@@ -7,14 +7,14 @@ public class Rectangle extends Quadrilateral implements Measurable {
     public Rectangle (double length, double width){
         super(length,width);
     }
-    @Override
-    public void setLength(double length){
-        this.length = length;
-    }
-    @Override
-    public void setWidth(double width){
-        this.width = width;
-    }
+//    @Override
+//    public void setLength(double length){
+//        this.length = length;
+//    }
+//    @Override
+//    public void setWidth(double width){
+//        this.width = width;
+//    }
     @Override
     public double getArea(){
         System.out.println("The area of the rectangle is: ");
--- src/Shapes/Square.java
+++ src/Shapes/Square.java
@@ -1,26 +1,26 @@
 package Shapes;
 
 
-public class Square extends Quadrilateral {
-    public Square (double side){
-        super(side, side);
-    }
+//public class Square extends Quadrilateral {
+//    public Square (double side){
+//        super(side, side);
+//    }
 
 
-@Override
-    public double getArea(){
-        return length * length;
-    }
-@Override
-    public double getPerimeter(){
-        return 4 * length;
-    }
-    @Override
-    public void setLength(double length){
-        this.length = length;
-    }
-    @Override
-    public void setWidth(double width){
-        this.width = width;
-    }
-}
+//@Override
+//    public double getArea(){
+//        return length * length;
+//    }
+//@Override
+//    public double getPerimeter(){
+//        return 4 * length;
+//    }
+//    @Override
+//    public void setLength(double length){
+//        this.length = length;
+//    }
+//    @Override
+//    public void setWidth(double width){
+//        this.width = width;
+//    }
+//}
