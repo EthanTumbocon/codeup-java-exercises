7a5c9a598793f763b896359cf44afe35aa686327
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2021-01-03 17:44:11 -0600

    finished the cart function and adding a cashier

--- src/JupiterMart/Cashier.java
+++ src/JupiterMart/Cashier.java
@@ -1,4 +1,24 @@
 package JupiterMart;
 
+import java.util.ArrayList;
+
 public class Cashier {
+    private String firstName;
+    private String lastName;
+
+    public Cashier(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void ringItem(Product item){
+        System.out.println("ID # " + item.getID() + " cost " + item.getPrice());
+    }
+    public double askForMoney(ArrayList<Product> cart){
+        double total = 0;
+        for(Product item : cart){
+            total += item.getPrice();
+        }
+        return total;
+    }
 }
--- src/JupiterMart/Mart.java
+++ src/JupiterMart/Mart.java
@@ -1,11 +1,29 @@
 package JupiterMart;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Mart {
     public static void main(String[] args){
         Product dell = new Computer(1, 1399.99, "Windows",true);
         Product iphone = new CellPhone(2,1200, 8,"IOS");
         Product PC = new Computer(3,1599.99, "Windows", false);
         Product android = new CellPhone(4,1299.99, 9, "Linux");
+    Cashier me = new Cashier("ethan", "tumbocon");
 
+        ArrayList<Product> cart = new ArrayList<>();
+
+        cart.add(iphone);
+        cart.add(dell);
+        cart.add(PC);
+        int idCounter = 0;
+        for(Product item : cart){
+            item.setID(idCounter);
+            idCounter++;
+        }
+        me.ringItem(dell);
+        me.ringItem(iphone);
+        me.ringItem(PC);
+        System.out.println("Your total is $" + me.askForMoney(cart));
     }
 }
